bd8400f52f1ba5726c4188d3eda203d1f4d344cb
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-08 16:14:54 -0200

    feat: create styles folder

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+export default {
+  white: '#FFF',
+  lighter: '#F2F2F2',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#111',
+  black: '#000',
+
+  primary: '#fbce09',
+  secundary: '#e7e7e7',
+
+  transparent: 'transparent',
+  whiteTransparent: 'rgba(255, 255, 255, 0.2)',
+  darkTransparent: 'rgba(0, 0, 0, 0.6)',
+  regularTransparent: 'rgba(0, 0, 0, 0.3)'
+};
