caf7cdca6f98da74c1762fbb7cfb990eff88a75e
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-09 01:59:34 -0200

    feat: set status bar white

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { ApolloProvider } from 'react-apollo';
+import { StatusBar } from 'react-native';
 import { Router } from '@router';
 import client from '@apollo/client';
 import { store } from '@redux/store';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    StatusBar.setBarStyle("light-content");
+  }
+
   render() {
     return (
       <ApolloProvider client={client}>
