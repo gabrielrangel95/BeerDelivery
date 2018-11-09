e097853583683acf34cc64910d186e2ce88a62c6
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-09 03:44:40 -0200

    feat: add handle add and remove

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -19,34 +19,56 @@ interface IProps {
   };
 }
 
-const ProductCard = (props: IProps) => {
-  const { item } = props;
-  if (!item.item) {
-    return(
+interface IState {
+  quantity: number;
+}
+
+class ProductCard extends React.PureComponent<IProps, IState> {
+  state = {
+    quantity: 0
+  };
+
+  handleAdd = () => {
+    const { quantity } = this.state;
+    this.setState({ quantity: quantity + 1 });
+  }
+
+  handleRemove = () => {
+    const { quantity } = this.state;
+    const newQuantity = quantity - 1 > 0 ? quantity - 1 : 0;
+    this.setState({ quantity: newQuantity });
+  }
+
+  render() {
+    const { item } = this.props;
+    const { quantity } = this.state;
+    if (!item.item) {
+      return (
+        <CardContainer>
+          <Loader color={colors.black} />
+        </CardContainer>
+      );
+    }
+    return (
       <CardContainer>
-        <Loader color={colors.black}/>
+        <ProductImage
+          resizeMode="cover"
+          source={{ uri: item.item.imageUrl }}
+        />
+        <ProductName numberOfLines={2}>{item.item.title}</ProductName>
+        <ProductPrice>R$ {item.item.price}</ProductPrice>
+        <QuantityContainer>
+          <ActionButton onPress={this.handleAdd}>
+            <MaterialIcons name="add" size={14} color={colors.black} />
+          </ActionButton>
+          <QuantityText>{quantity}</QuantityText>
+          <ActionButton  onPress={this.handleRemove}>
+            <MaterialIcons name="remove" size={14} color={colors.black} />
+          </ActionButton>
+        </QuantityContainer>
       </CardContainer>
     );
   }
-  return(
-    <CardContainer>
-      <ProductImage
-        resizeMode="cover"
-        source={{ uri: item.item.imageUrl }}
-      />
-      <ProductName numberOfLines={2}>{item.item.title}</ProductName>
-      <ProductPrice>R$ {item.item.price}</ProductPrice>
-      <QuantityContainer>
-        <ActionButton>
-          <MaterialIcons name="add" size={14} color={colors.black}/>
-        </ActionButton>
-        <QuantityText>0</QuantityText>
-        <ActionButton>
-          <MaterialIcons name="remove" size={14} color={colors.black}/>
-        </ActionButton>
-      </QuantityContainer>
-    </CardContainer>
-  );
 };
 
 export { ProductCard };
