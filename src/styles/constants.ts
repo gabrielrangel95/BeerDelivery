a64541a5d6a3b3f5abac31fbb1c6bbe94c521d98
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-09 03:38:23 -0200

    feat: new constants

--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -1,4 +1,13 @@
 import { metrics } from './index';
 
+// components
+  // button
 export const BUTTON_HEIGHT = 42;
 export const BUTTON_WIDTH = metrics.SCREEN_WIDTH * 0.4;
+  // product card
+export const PRODUCT_CARD_WIDTH =  metrics.SCREEN_WIDTH * 0.4;
+export const PRODUCT_CARD_HEIGHT = PRODUCT_CARD_WIDTH * 1.4;
+export const PRODUCT_CARD_ACTION_BUTTON_SIZE = 28;
+
+// products screen
+export const CATEGORIES_CONTAINER_HEIGHT = metrics.SCREEN_HEIGHT * 0.05;
