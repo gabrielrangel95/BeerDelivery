436d446d41be4c273d696a58a528dc3609880b98
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-08 23:30:36 -0200

    feat: connect redux to home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,21 +1,99 @@
 import * as React from 'react';
+import { Keys } from '@keys';
+
+// redux
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PocActions } from '@redux/actions/poc';
+
+// interfaces
+import { IDispatchToProps, IStateToProps } from '@interfaces/poc';
+import { IReduxState } from '@interfaces/reduxState';
+
+// components
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+// ui
 import {
   MainContainer,
-  MainText
+  Subtitle,
+  Title,
+  InputStyle
 } from './HomeStyle';
 
-class Home extends React.PureComponent<any, any> {
+// utils
+const defaultPlace = { description: 'PoC', geometry: { location: { lat: -23.632919, lng: -46.699453 } } };
+
+type IProps = IStateToProps & IDispatchToProps;
+
+interface IState {
+  lat: number | undefined;
+  long: number | undefined;
+}
+
+class Home extends React.PureComponent<IProps, IState> {
   static navigationOptions = {
     header: null
   };
 
+  state = {
+    lat: undefined,
+    long: undefined
+  };
+
+  handleLocationPress = (data) => {
+    const { location } = data.geometry;
+    this.setState({
+      lat: location.lat,
+      long: location.lng
+    });
+    this.props.getPocRequest({ lat: location.lat, long: location.lng});
+  }
+
   render() {
-    return(
+    console.log(this.props);
+    return (
       <MainContainer>
-        <MainText>Beer Delivery</MainText>
+        <Title>Beer Delivery.</Title>
+        <Subtitle>Bebida gelada, rápida a preço baixo</Subtitle>
+        <GooglePlacesAutocomplete
+          placeholder='Endereço de Entrega'
+          minLength={3}
+          autoFocus={false}
+          returnKeyType={'search'}
+          listViewDisplayed='auto'
+          fetchDetails={true}
+          renderDescription={row => row.description} // custom description render
+          onPress={(data) => this.handleLocationPress(data) }
+          getDefaultValue={() => ''}
+          query={{
+            key: Keys.MapsApiKey,
+            language: 'pt',
+            types: '(address)'
+          }}
+          styles={InputStyle}
+          currentLocation={true}
+          currentLocationLabel="Localização Atual"
+          nearbyPlacesAPI='GooglePlacesSearch'
+          predefinedPlaces={[defaultPlace]}
+          debounce={200}
+        />
       </MainContainer>
     );
   }
 }
 
-export { Home };
+const mapStateToProps = (state) => ({
+  data: state.poc.data,
+  loading: state.poc.loading,
+  getFailure: state.poc.getFailure
+});
+
+const mapDispatchToProps = (dispatch: any) => bindActionCreators({ ...PocActions }, dispatch);
+
+const HomeConnected = connect<IStateToProps, IDispatchToProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
+
+export { HomeConnected as Home };
--- src/screens/Home/HomeStyle.ts
+++ src/screens/Home/HomeStyle.ts
@@ -5,10 +5,39 @@ export const MainContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  background-color: ${colors.white};
+  background-color: ${colors.black};
+  padding: 24px 12px;
 `;
 
 export const MainText = styled.Text`
   font-size: 24;
-  color: ${colors.black};
+  color: ${colors.white};
 `;
+
+export const Title = styled.Text`
+  font-size: 32;
+  font-weight: 700;
+  color: ${colors.white};
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 16;
+  color: ${colors.white};
+`;
+
+export const InputStyle = {
+  container: {
+    borderColor: 'transparent'
+  },
+  textInputContainer: {
+    width: '80%',
+    backgroundColor: colors.white,
+    borderRadius: '6%'
+  },
+  description: {
+    fontWeight: 'bold'
+  },
+  predefinedPlacesDescription: {
+    color: colors.primary
+  }
+};
