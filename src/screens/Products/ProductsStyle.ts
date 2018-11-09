4bda551be5c329c7b740eb1467ba63c99cd713ef
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-09 03:38:16 -0200

    feat: product list style

--- src/screens/Products/ProductsStyle.ts
+++ src/screens/Products/ProductsStyle.ts
@@ -1,12 +1,20 @@
 import styled from 'styled-components/native';
-import { colors } from '@styles';
+import { colors, constants } from '@styles';
 
 export const MainContainer = styled.View`
   flex: 1;
   align-items: center;
+  justify-content: flex-start;
+  background-color: ${colors.white};
+  padding-left: 12px;
+  padding-right: 12px;
+`;
+
+export const LoaderContainer = styled.View`
+  flex: 1;
+  align-items: center;
   justify-content: center;
   background-color: ${colors.white};
-  padding: 24px 12px;
 `;
 
 export const Title = styled.Text`
@@ -14,3 +22,14 @@ export const Title = styled.Text`
   font-weight: 700;
   color: ${colors.black};
 `;
+
+export const CategoriesContainer = styled.View`
+  display: flex;
+  height: ${constants.CATEGORIES_CONTAINER_HEIGHT};
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: ${colors.lighter};
+`;
+
+export const Loader = styled.ActivityIndicator``;
