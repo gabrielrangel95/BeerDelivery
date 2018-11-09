b11a4b85431da09c315e5199b014b42bdac78032
Gabriel Rangel <gabriel.avila.rangel@gmail.com>
2018-11-08 23:30:23 -0200

    feat: implement redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import { ApolloProvider } from 'react-apollo';
 import { Router } from '@router';
 import client from '@apollo/client';
+import { store } from '@redux/store';
 
 export default class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Router />
+        <Provider store={store}>
+          <Router />
+        </Provider>
       </ApolloProvider>
     );
   }
